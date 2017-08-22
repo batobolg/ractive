5cc7c7afa1d00f86c891c4440f8888d5d447db2d
Joseph <fskreuz@gmail.com>
2017-08-22 19:28:55 -0400

    Delete the old structure

--- src/polyfills/performance.now.js
+++
@@ -1,9 +0,0 @@
-if (typeof window !== 'undefined' && window.performance && !window.performance.now) {
-	window.performance = window.performance || {};
-
-	const nowOffset = Date.now();
-
-	window.performance.now = function () {
-		return Date.now() - nowOffset;
-	};
-}
